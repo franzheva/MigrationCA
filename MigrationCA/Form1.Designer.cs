63644425f5cc206c740378b0926fcd9aa2c1d648
Olena Franzheva GG <olena.franzheva@geekgroupllc.com>
2021-01-19 22:09:06 +0200

    input validation

--- MigrationCA/Form1.Designer.cs
+++ MigrationCA/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MigrationCA
 {
-    partial class Form1
+    partial class MigrationMainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,59 +30,162 @@
         {
             this.components = new System.ComponentModel.Container();
             this.PanelForSettings = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ValidationLbl = new System.Windows.Forms.Label();
+            this.ScaleTbx = new System.Windows.Forms.TextBox();
+            this.WidthTbx = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.HeightTbx = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.IterationLbl = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.SettingsBtn = new System.Windows.Forms.Button();
             this.StartStopBtn = new System.Windows.Forms.Button();
             this.DrawingPanel = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.PanelForSettings.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // PanelForSettings
             // 
+            this.PanelForSettings.Controls.Add(this.panel1);
             this.PanelForSettings.Controls.Add(this.IterationLbl);
             this.PanelForSettings.Controls.Add(this.label1);
-            this.PanelForSettings.Controls.Add(this.SettingsBtn);
             this.PanelForSettings.Controls.Add(this.StartStopBtn);
             this.PanelForSettings.Location = new System.Drawing.Point(0, 0);
             this.PanelForSettings.Name = "PanelForSettings";
-            this.PanelForSettings.Size = new System.Drawing.Size(1251, 104);
+            this.PanelForSettings.Size = new System.Drawing.Size(1251, 74);
             this.PanelForSettings.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.ValidationLbl);
+            this.panel1.Controls.Add(this.ScaleTbx);
+            this.panel1.Controls.Add(this.WidthTbx);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.HeightTbx);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(366, 74);
+            this.panel1.TabIndex = 7;
+            // 
+            // ValidationLbl
+            // 
+            this.ValidationLbl.AutoSize = true;
+            this.ValidationLbl.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ValidationLbl.ForeColor = System.Drawing.Color.Red;
+            this.ValidationLbl.Location = new System.Drawing.Point(83, 51);
+            this.ValidationLbl.Name = "ValidationLbl";
+            this.ValidationLbl.Size = new System.Drawing.Size(0, 19);
+            this.ValidationLbl.TabIndex = 0;
+            // 
+            // ScaleTbx
+            // 
+            this.ScaleTbx.ForeColor = System.Drawing.Color.Silver;
+            this.ScaleTbx.Location = new System.Drawing.Point(302, 28);
+            this.ScaleTbx.Name = "ScaleTbx";
+            this.ScaleTbx.Size = new System.Drawing.Size(60, 23);
+            this.ScaleTbx.TabIndex = 9;
+            this.ScaleTbx.Text = "Scale";
+            this.ScaleTbx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ScaleTbx.Leave += new System.EventHandler(this.ScaleTbx_Leave);
+            this.ScaleTbx.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ScaleTbx_MouseDown);
+            // 
+            // WidthTbx
+            // 
+            this.WidthTbx.ForeColor = System.Drawing.Color.Silver;
+            this.WidthTbx.Location = new System.Drawing.Point(182, 28);
+            this.WidthTbx.Name = "WidthTbx";
+            this.WidthTbx.Size = new System.Drawing.Size(60, 23);
+            this.WidthTbx.TabIndex = 9;
+            this.WidthTbx.Text = "Width";
+            this.WidthTbx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.WidthTbx.Leave += new System.EventHandler(this.WidthTbx_Leave);
+            this.WidthTbx.MouseDown += new System.Windows.Forms.MouseEventHandler(this.WidthTbx_MouseDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(253, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Scale:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(128, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Width:";
+            // 
+            // HeightTbx
+            // 
+            this.HeightTbx.ForeColor = System.Drawing.Color.Silver;
+            this.HeightTbx.Location = new System.Drawing.Point(62, 28);
+            this.HeightTbx.Name = "HeightTbx";
+            this.HeightTbx.Size = new System.Drawing.Size(60, 23);
+            this.HeightTbx.TabIndex = 0;
+            this.HeightTbx.Text = "Height";
+            this.HeightTbx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.HeightTbx.Leave += new System.EventHandler(this.HeightTbx_Leave);
+            this.HeightTbx.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HeightTbx_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(3, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Height:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(132, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "CA field setup";
             // 
             // IterationLbl
             // 
             this.IterationLbl.AutoSize = true;
             this.IterationLbl.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.IterationLbl.Location = new System.Drawing.Point(190, 15);
+            this.IterationLbl.Location = new System.Drawing.Point(565, 18);
             this.IterationLbl.Name = "IterationLbl";
             this.IterationLbl.Size = new System.Drawing.Size(17, 19);
-            this.IterationLbl.TabIndex = 3;
+            this.IterationLbl.TabIndex = 4;
             this.IterationLbl.Text = "0";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(120, 15);
+            this.label1.Location = new System.Drawing.Point(484, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 19);
-            this.label1.TabIndex = 3;
+            this.label1.TabIndex = 5;
             this.label1.Text = "Iteration:";
-            // 
-            // SettingsBtn
-            // 
-            this.SettingsBtn.Location = new System.Drawing.Point(1, 0);
-            this.SettingsBtn.Name = "SettingsBtn";
-            this.SettingsBtn.Size = new System.Drawing.Size(116, 50);
-            this.SettingsBtn.TabIndex = 2;
-            this.SettingsBtn.Text = "Settings";
-            this.SettingsBtn.UseVisualStyleBackColor = true;
-            this.SettingsBtn.Click += new System.EventHandler(this.StartStopBtn_Click);
             // 
             // StartStopBtn
             // 
-            this.StartStopBtn.Location = new System.Drawing.Point(1, 53);
+            this.StartStopBtn.Location = new System.Drawing.Point(367, 2);
             this.StartStopBtn.Name = "StartStopBtn";
             this.StartStopBtn.Size = new System.Drawing.Size(116, 50);
             this.StartStopBtn.TabIndex = 2;
@@ -92,28 +195,30 @@
             // 
             // DrawingPanel
             // 
-            this.DrawingPanel.Location = new System.Drawing.Point(0, 110);
+            this.DrawingPanel.Location = new System.Drawing.Point(0, 80);
             this.DrawingPanel.Name = "DrawingPanel";
-            this.DrawingPanel.Size = new System.Drawing.Size(1251, 352);
+            this.DrawingPanel.Size = new System.Drawing.Size(1251, 382);
             this.DrawingPanel.TabIndex = 1;
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // Form1
+            // MigrationMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 474);
             this.Controls.Add(this.DrawingPanel);
             this.Controls.Add(this.PanelForSettings);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MigrationMainForm";
+            this.Text = "Migration";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.PanelForSettings.ResumeLayout(false);
             this.PanelForSettings.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -124,9 +229,18 @@
         private System.Windows.Forms.Panel DrawingPanel;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button StartStopBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ValidationLbl;
+        private System.Windows.Forms.TextBox ScaleTbx;
+        private System.Windows.Forms.TextBox WidthTbx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox HeightTbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label IterationLbl;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button SettingsBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- MigrationCA/Form1.cs
+++ MigrationCA/Form1.cs
@@ -5,18 +5,23 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MigrationCA
 {
-    public partial class Form1 : Form
+    public partial class MigrationMainForm : Form
     {
         public bool isFirstLaunch = true;
         public bool AllowRunMigration = true;
         public int IterationCounter = 0;
+        public bool isValid = false;
+        public int CaWidth = 0;
+        public int CaHeight = 0; 
+        public int CaScale=0;
         PictureBox migrationPictureBox = new PictureBox();
-        public Form1()
+        public MigrationMainForm()
         {
             InitializeComponent();
         }
@@ -50,40 +55,105 @@ namespace MigrationCA
         {
             if (AllowRunMigration)
             {
-                if (isFirstLaunch)
+                SettingsValidation();
+                if (isValid)
                 {
+                    if (isFirstLaunch)
+                    {
+                        int.TryParse(HeightTbx.Text, out CaHeight);
+                        int.TryParse(WidthTbx.Text, out CaWidth);
+                        int.TryParse(ScaleTbx.Text, out CaScale);
+                        timer1.Enabled = true;
+                        isFirstLaunch = false;
+                    }
                     timer1.Enabled = true;
-                    isFirstLaunch = false;
+                    AllowRunMigration = false;
+                    HeightTbx.ReadOnly = true;
+                    WidthTbx.ReadOnly = true;
+                    ScaleTbx.ReadOnly = true;
                 }
-                timer1.Enabled = true;
-                AllowRunMigration = false;
             }
-            else {
+            else
+            {
                 timer1.Enabled = false;
+                HeightTbx.ReadOnly = false;
+                WidthTbx.ReadOnly = false;
+                ScaleTbx.ReadOnly = false;
                 AllowRunMigration = true;
             }
+            
         }
         public void CreateBitmapAtRuntime(decimal[,] A)
-        {
-           var  HeightField = f.HeightImg;
-            WidthField = f.WidthImg;
-            var scale = f.scale;
-            migrationPictureBox.Size = new Size(WidthField * scale + 10, HeightField * scale + 10);
-            
+        {            
+            migrationPictureBox.Size = new Size(CaWidth * CaScale + 10, CaHeight * CaScale + 10);
             DrawingPanel.Controls.Add(migrationPictureBox);
-            Bitmap myAutomataField = new Bitmap(WidthField * scale, HeightField * scale);
+            Bitmap myAutomataField = new Bitmap(CaWidth * CaScale, CaHeight * CaScale);
             Graphics flagGraphics = Graphics.FromImage(myAutomataField);
-            for (int j = 0; j < WidthField; j++)
+            for (int j = 0; j < CaWidth; j++)
             {
-                for (int i = 0; i < HeightField; i++)
+                for (int i = 0; i < CaHeight; i++)
                 {
                     int col = Convert.ToInt32((1 - A[i, j]) * 255); //Math.Abs(1 - A[j, i]) old version
-                    flagGraphics.FillRectangle(new SolidBrush(Color.FromArgb(col, col, col)), j * scale, i * scale, scale, scale); //(col, col, col) try to (col, 0, 0)
+                    flagGraphics.FillRectangle(new SolidBrush(Color.FromArgb(col, col, col)), j * CaScale, i * CaScale, CaScale, CaScale); //(col, col, col) try to (col, 0, 0)
                 }
             }
 
             migrationPictureBox.Image = myAutomataField;
-            
+
+        }
+
+        public void SettingsValidation()
+        {
+            ValidationLbl.Text = (HeightTbx.Text == string.Empty || HeightTbx.Text == "Height")
+                || (WidthTbx.Text == string.Empty || WidthTbx.Text == "Width")
+                || (ScaleTbx.Text == string.Empty || ScaleTbx.Text == "Scale")
+                ? "Empty fields are not allowed"
+                    : (!Regex.IsMatch(HeightTbx.Text, @"^[0-9]*?(\,)?[0-9]*$")
+                    || !Regex.IsMatch(WidthTbx.Text, @"^[0-9]*?(\,)?[0-9]*$")
+                    || !Regex.IsMatch(ScaleTbx.Text, @"^[0-9]*?(\,)?[0-9]*$"))  
+                        ? "Only digitals are allowed"
+                        :string.Empty;
+           
+            isValid = ValidationLbl.Text == string.Empty;
+        }
+
+        private void HeightTbx_MouseDown(object sender, MouseEventArgs e)
+        {
+            HeightTbx.Text = (HeightTbx.Text == string.Empty || HeightTbx.Text == "Height")? string.Empty:HeightTbx.Text;
+            HeightTbx.ForeColor = Color.Black;
+        }       
+
+        private void HeightTbx_Leave(object sender, EventArgs e)
+        {
+            HeightTbx.ForeColor = HeightTbx.Text == string.Empty ? Color.Silver : Color.Black;
+            HeightTbx.Text = HeightTbx.Text == string.Empty ? "Height" : HeightTbx.Text;
+            isFirstLaunch = HeightTbx.Text != CaHeight.ToString();
+        }
+
+        private void WidthTbx_Leave(object sender, EventArgs e)
+        {
+            WidthTbx.ForeColor = WidthTbx.Text == string.Empty ? Color.Silver : Color.Black;
+            WidthTbx.Text = WidthTbx.Text == string.Empty ? "Width" : WidthTbx.Text;
+            isFirstLaunch = WidthTbx.Text != CaWidth.ToString();
+        }
+
+        private void WidthTbx_MouseDown(object sender, MouseEventArgs e)
+        {
+            WidthTbx.Text = (WidthTbx.Text == string.Empty || WidthTbx.Text == "Width") ? string.Empty : WidthTbx.Text;
+            WidthTbx.ForeColor = Color.Black;
+        }
+
+        private void ScaleTbx_MouseDown(object sender, MouseEventArgs e)
+        {
+            ScaleTbx.Text = (ScaleTbx.Text == string.Empty || ScaleTbx.Text == "Scale") ? string.Empty : ScaleTbx.Text;
+            ScaleTbx.ForeColor = Color.Black;
+        }
+
+        private void ScaleTbx_Leave(object sender, EventArgs e)
+        {
+            ScaleTbx.ForeColor = ScaleTbx.Text == string.Empty ? Color.Silver : Color.Black;
+            ScaleTbx.Text = ScaleTbx.Text == string.Empty ? "Scale" : ScaleTbx.Text;
+            isFirstLaunch = ScaleTbx.Text != CaScale.ToString();
         }
     }
 }
