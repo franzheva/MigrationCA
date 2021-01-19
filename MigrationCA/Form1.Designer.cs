ed9a068c746e48d985e0eb957f6008122a2999a2
franzheva <40060461+franzheva@users.noreply.github.com>
2021-01-19 13:58:21 +0200

    Initial commit

---
+++ MigrationCA/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MigrationCA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PanelForSettings = new System.Windows.Forms.Panel();
+            this.DrawingPanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartStopBtn = new System.Windows.Forms.Button();
+            this.PanelForSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelForSettings
+            // 
+            this.PanelForSettings.Controls.Add(this.StartStopBtn);
+            this.PanelForSettings.Location = new System.Drawing.Point(0, 0);
+            this.PanelForSettings.Name = "PanelForSettings";
+            this.PanelForSettings.Size = new System.Drawing.Size(1251, 104);
+            this.PanelForSettings.TabIndex = 0;
+            // 
+            // DrawingPanel
+            // 
+            this.DrawingPanel.Location = new System.Drawing.Point(0, 110);
+            this.DrawingPanel.Name = "DrawingPanel";
+            this.DrawingPanel.Size = new System.Drawing.Size(1251, 352);
+            this.DrawingPanel.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StartStopBtn
+            // 
+            this.StartStopBtn.Location = new System.Drawing.Point(0, 0);
+            this.StartStopBtn.Name = "StartStopBtn";
+            this.StartStopBtn.Size = new System.Drawing.Size(116, 58);
+            this.StartStopBtn.TabIndex = 2;
+            this.StartStopBtn.Text = "Start/Stop Migration";
+            this.StartStopBtn.UseVisualStyleBackColor = true;
+            this.StartStopBtn.Click += new System.EventHandler(this.StartStopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 474);
+            this.Controls.Add(this.DrawingPanel);
+            this.Controls.Add(this.PanelForSettings);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.PanelForSettings.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelForSettings;
+        private System.Windows.Forms.Panel DrawingPanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartStopBtn;
+    }
+}
+
---
+++ MigrationCA/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MigrationCA
+{
+    public partial class Form1 : Form
+    {
+        public bool isFirstLaunch = true;
+        public bool AllowRunMigration = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PanelForSettings_Position();
+        }
+        public void PanelForSettings_Position()
+        {
+            PanelForSettings.Width = this.Width;
+            DrawingPanel.Location = new System.Drawing.Point(0, PanelForSettings.Height);
+            DrawingPanel.Width = this.Width;
+            DrawingPanel.Height = this.Height - PanelForSettings.Height;
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            PanelForSettings_Position();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //call migration recalculation
+        }
+
+        private void StartStopBtn_Click(object sender, EventArgs e)
+        {
+            if (AllowRunMigration)
+            {
+                if (isFirstLaunch)
+                {
+                    timer1.Enabled = true;
+                    isFirstLaunch = false;
+                }
+                timer1.Enabled = true;
+                AllowRunMigration = false;
+            }
+            else {
+                timer1.Enabled = false;
+                AllowRunMigration = true;
+            }
+        }
+    }
+}
